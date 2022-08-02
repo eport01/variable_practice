47bbb1963a3f40eff2ddbb79757ad741da469200
Emily Port <emilylport@gmail.com>
2022-08-02 14:05:58 -0600

    add a puts statement

--- variables.rb
+++ variables.rb
@@ -1,6 +1,6 @@
-name = "Emily Port"
-dog_name = "Louie"
-cat_name = "Addie"
+name = "Emily P."
+dog_name = "Louie the lab"
+cat_name = "Zoe"
 my_age = 30
 dog_age = 3
 cat_age = 1
@@ -8,3 +8,4 @@ height = 162.56
 current_income = 0.000
 have_dog = true
 have_cat = true
+puts "Hi I'm " name "and I am" my_age "years old."
